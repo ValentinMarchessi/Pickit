7c5d43345985faf8f4afd111ff2d791bdf70122e
Valentín Marchessi <vmarchessi@gmail.com>
2022-03-12 20:35:14 -0300

    type fixes

--- src/Components/Input/Input.tsx
+++ src/Components/Input/Input.tsx
@@ -3,7 +3,7 @@ import styles from './Input.module.scss';
 
 type MyInputProps = {
     name: string,
-    type: 'text' | 'number' | 'password' | 'email'
+    type: 'text' | 'number' | 'password' | 'email' | 'date'
     onBlur?: React.FocusEventHandler,
     onChange?: React.ChangeEventHandler,
     label: string,
@@ -12,24 +12,24 @@ type MyInputProps = {
 export default function Input({ label, onBlur, onChange, name, type }: MyInputProps) {
     const [value, setValue] = useState('')
     
-    function handleChange(event: React.ChangeEvent) {
-        const { value } = event.target;
+    function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+        const { value } = event.currentTarget;
         setValue(value);
         onChange && onChange(event);
     }
 
-    function handleBlur(event: React.FocusEvent) {
-		const { value } = event.target;
+    function handleBlur(event: React.FocusEvent<HTMLInputElement>) {
+		const { value } = event.currentTarget;
 		setValue(value);
 		onBlur && onBlur(event);
 	}
 
 	return (
 		<div className={styles.container}>
-			<label className={value ? styles.active : undefined} htmlFor={name}>
+			<label className={value || type==="date" ? styles.active : undefined} htmlFor={name}>
 				{label}
 			</label>
-			<input value={value} name={name} onBlur={handleBlur} onChange={handleChange} type={type} />
+			<input value={value} name={name} onBlur={handleBlur} onChange={handleChange} type={type} autoComplete="off" />
 		</div>
 	);
 }
