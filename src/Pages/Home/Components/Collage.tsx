556dcf6e50e18c1cb852256f92f4d28cdc3455cc
Valentín Marchessi <vmarchessi@gmail.com>
2022-03-16 09:09:59 -0300

    optimization tweak

--- src/Pages/Home/Components/Collage.tsx
+++ src/Pages/Home/Components/Collage.tsx
@@ -7,7 +7,7 @@ export default function Collage() {
 	function infiniteScroll(event: React.UIEvent<HTMLDivElement>) {
 		const container = event.currentTarget;
 		const images = container.children;
-		for (var i = 0; i < images.length; i++) {
+		for (var i = 0; i <= 2; i++) {
 			const { y } = images[i].getBoundingClientRect();
 			if (y < 0) {
 				container.append(images[i]);
