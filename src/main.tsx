f129f45bdf2b2aa838dff7ba380a701792627b81
Valentín Marchessi <vmarchessi@gmail.com>
2022-03-16 09:08:26 -0300

    debug

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,23 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Login, Register } from "./Components";
+import { Login, Register, Picture } from "./Components";
 import { Landing, Debug, Home } from "./Pages";
 import { persistor, store } from "./Redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { Provider } from "react-redux";
+
+import man from './Assets/images/man.jpeg';
+import balloon from './Assets/images/balloon.jpg';
+
+const pictureProps = {
+	author: {
+		name: "Richard",
+		avatar: man,
+	},
+	title: "Balloon",
+	src: balloon,
+};
 
 ReactDOM.render(
 	<React.StrictMode>
@@ -17,13 +29,13 @@ ReactDOM.render(
 					<Routes>
 						<Route path="/" element={<App />}>
 							<Route index element={<Landing />} />
-							<Route path="home" element={<Home/>}/>
+							<Route path="home" element={<Home />} />
 						</Route>
-						<Route path="/debug" element={<Debug component={<Login/>} />} />
+						<Route path="/debug" element={<Debug component={<Picture {...pictureProps} />} />} />
 					</Routes>
 				</BrowserRouter>
 			</PersistGate>
 		</Provider>
 	</React.StrictMode>,
-	document.getElementById("root")
+	document.getElementById("app-root")
 );
